b2c2668cb8c2211457b5e68f69d4e6995f6564b0
mlwelker <nutria.watts.0g@icloud.com>
2023-10-29 14:38:25 -0500

    Initial Commit

---
+++ iExpenseTCA/iExpenseTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseTCAApp.swift
+//  iExpenseTCA
+//
+//  Created by Michael Welker on 2023-10-29.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
